e8fa870705d55583580aff2cae4040d80321fc7d
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-13 19:31:41 +0530

    Finish test suite successfully

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
     let result = "";
     for(let i = 1; i <= num; i++) {
         result += string;
     }
     return result;
+}
 };
 
 // Do not edit below this line
