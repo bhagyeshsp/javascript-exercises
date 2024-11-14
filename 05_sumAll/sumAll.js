f799b4788594e5f085aaec11987d82140b24b252
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-14 14:05:47 +0530

    Complete all tests
    The rest of the tests were comparatively easy. The learning lesson for me was the opportunity to carry out meanigful reading and implementation from the developer documentation about different properties and types.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,28 @@
-const sumAll = function(arg1, arg2) {
-    let num1 = 0;
-    let num2 = 0;
+const sumAll = function (arg1, arg2) {
+  let num1 = 0;
+  let num2 = 0;
+  if (arg1 < 0 || arg2 < 0) {
+    return "ERROR";
+  } else if (arg1 != Math.floor(arg1) || arg2 != Math.floor(arg2)) {
+    return "ERROR";
+  } else if (arg1 === NaN || arg2 === NaN) {
+    return "ERROR";
+  } else if (typeof arg1 === "string" || typeof arg2 === "string") {
+    return "ERROR";
+  } else {
     if (arg1 > arg2) {
-        num1 = arg2;
-        num2 = arg1;
+      num1 = arg2;
+      num2 = arg1;
     } else {
-        num1 = arg1;
-        num2 = arg2;
-    }    
+      num1 = arg1;
+      num2 = arg2;
+    }
     let sum = 0;
-    for(let i = num1; i <= num2; i++ ) {
-        sum += i;
+    for (let i = num1; i <= num2; i++) {
+      sum += i;
     }
     return sum;
+  }
 };
 
 // Do not edit below this line
