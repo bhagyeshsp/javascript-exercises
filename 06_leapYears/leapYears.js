a5c1daa928867d6367d3c6b506a453181807a633
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-16 22:40:20 +0530

    Complete all tests
    The leap year calculation instruction was unnecessarily complicated. The struggle to understand the parameters of calculating leap years made me write wrong code after the first test. It wasn't until I googled, I learned about the nested rules of the leap year calculation.
    
    The solution code is quite elegant. My code is not that elegant but the learning was good.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,23 @@
-const leapYears = function(inputYear) {
-    if (inputYear !== null) {
-        if (inputYear % 4 === 0) {
-            return true;
-        } else if (inputYear % 4 === 0 && inputYear % 100 === 0) {
-            return true;
-        } else {
-            return false;
-        }
+const leapYears = function (inputYear) {
+  if (inputYear !== null) {
+    if (inputYear % 4 !== 0) {
+      return false;
+    } else if (inputYear % 4 === 0 && inputYear % 100 !== 0) {
+      return true;
+    } else if (
+      inputYear % 4 === 0 &&
+      inputYear % 100 === 0 &&
+      inputYear % 400 === 0
+    ) {
+      return true;
+    } else if (
+      inputYear % 4 === 0 &&
+      inputYear % 100 === 0 &&
+      inputYear % 400 !== 0
+    ) {
+      return false;
     }
+  }
 };
 
 // Do not edit below this line
