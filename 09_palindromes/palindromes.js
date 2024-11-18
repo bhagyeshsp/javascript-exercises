80e86cd8efdd9374764f03a2b04fc1b8b9315c94
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-18 13:48:01 +0530

    Complete all tests.
    The solution code is unnecessarily difficult. I googled the removal of punctuation marks and spaces and received the necessary regex code for replacement. The method I've undertaken, uses minimum amount of chained functions. Also, the solution code seems smart, but I don't know how the code would work if the input characters were non-english alphabets. This is not a commentary on comparision with the solution code, but since I just had a look at it, I'm thinking out loud.
    
    This exercise was fun because it allowed to think in layers one after another.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (originalWord) {
+    originalWord = originalWord.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g,"");
+    originalWord = originalWord.replace(/\s{1,}/g,"");
+    let mirrorWord = "";
+    let isPalindrome = "";
+    for(let i = originalWord.length-1; i >= 0; i--) { 
+        mirrorWord += originalWord.charAt(i);
+    }
+    isPalindrome = (mirrorWord === originalWord);
+    return isPalindrome;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+// console.log(palindromes("Animal loots foliated detail of stool lamina."));
