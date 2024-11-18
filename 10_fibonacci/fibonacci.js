49fac368763fbb44ed0f5312d3b9e88469db5fc4
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-18 18:12:49 +0530

    Completed all tests
    Setting up the code for the initial test was a bit challenging and the wikipedia formula initially threw me in unnecessary mathematician role. But shortly, I wrote pseudocode and things became clear.
    
    The nature of requirement with a one place displaced index threw an error but solving that taught about paying attention and being present about the workings of the code.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = Number(num);
+  let result = [1, 1];
+  let appendElement = "";
+  // Suppose we want to generate fibonacci sequence [1, 1, 2, 3, 5, 8,]
+  // Seed the array with the first two values [1,1]
+  // Begin a loop
+  // Loop generates the next number by adding index-1 and index-2 numbers
+  // Loop stops when the index reaches the provided number
+  if (num === 0) return 0;
+  else if (num < 0) return "OOPS";
+  else {
+    for (let i = 2; i <= num; i++) {
+      appendElement = result[i - 1] + result[i - 2];
+      result.push(appendElement);
+    }
+    return result[num - 1];
+  }
 };
 
 // Do not edit below this line
