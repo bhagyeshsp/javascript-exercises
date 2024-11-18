8e88f1896f1552f61bbca9004fb614a792c04fe7
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-18 22:00:47 +0530

    Completed the exercise
    Dealing with objects in the array was new for me. There seemed many ways to accomplish this but I wasn't sure.
    
    Finally, googling helped. The solution of the exercise has only array.map() method. But as I learned from the online reference, adding filter() method is more useful in filtering out objects that do not have title in them.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let result = [];
+    result = books.filter(book => book.title).map(book => book.title);
+    return result;
 };
 
 // Do not edit below this line
