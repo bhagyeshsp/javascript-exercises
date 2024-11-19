64469171962cd9a69bad4ae9bc1a43989cb992f5
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-19 15:01:39 +0530

    Completed second test
    This was interesting as I learned about using current time using JS.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,10 +2,13 @@ const findTheOldest = function (people) {
   let prevValue = 0;
   let result = "Ab";
   let newArray = [];
-  people.forEach(
-    (person) => {person.age = person.yearOfDeath - person.yearOfBirth;
+  people.forEach((person) => {
+    if (person.yearOfDeath === null) {
+      person.yearOfDeath = new Date();
+    }
+    person.age = person.yearOfDeath - person.yearOfBirth;
     newArray.push(person);
-    });
+  });
   for (const person of newArray) {
     if (person.age > prevValue) {
       prevValue = person.age;
