c2bbc9da471d0ee1941a7296e4e97e846b5fb21a
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-19 14:47:40 +0530

    Complete first test
    This was the longest it took me to come to the right code to get the result.
    
    In a debugger, I had the code return Ray as expected, but while jest testing, the test repeatedly returned undfined. It was frustrating. Finally, reading the test equality check, I realized, the test was comparing the return array's name key's value. So, instead of returning person.name, I passed person to result. This worked.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let prevValue = 0;
+  let result = "Ab";
+  let newArray = [];
+  people.forEach(
+    (person) => {person.age = person.yearOfDeath - person.yearOfBirth;
+    newArray.push(person);
+    });
+  for (const person of newArray) {
+    if (person.age > prevValue) {
+      prevValue = person.age;
+      result = person;
+    } else {
+      continue;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
